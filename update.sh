721d31ba07fddf5e113e601404f19aabe75dbeb6
Antonin Décimo <antonin@tarides.com>
2023-03-13 18:12:07 +0100

    Add an update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+command -v curl 2>&1 >/dev/null || exit
+command -v jq 2>&1 >/dev/null || exit
+command -v git 2>&1 >/dev/null || exit
+
+url="https://github.com/tailwindlabs/tailwindcss/releases/latest/download"
+
+cd bin || exit
+curl -LO "${url}/tailwindcss-linux-arm64"
+curl -LO "${url}/tailwindcss-linux-x64"
+curl -LO "${url}/tailwindcss-macos-arm64"
+curl -LO "${url}/tailwindcss-macos-x64"
+curl -LO "${url}/tailwindcss-windows-x64.exe"
+cd .. || exit
+
+tag_name=$(curl -s https://api.github.com/repos/tailwindlabs/tailwindcss/releases/latest | jq -r .tag_name)
+
+curl -L "https://raw.githubusercontent.com/tailwindlabs/tailwindcss/$tag_name/CHANGELOG.md" -o CHANGES.md
+
+git add CHANGES.md bin
+git commit -m "Upgrade to tailwind $tag_name"
